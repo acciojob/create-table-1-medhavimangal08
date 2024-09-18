a0bb4cf571e3cadccde547e665d76f9918b48191
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-19 00:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
+	let table=document.querySelector("#sampleTable");
+	let tr=table.insertRow(0);
+	let cell1=tr.insertCell(0);
+	let cell2=tr.insertCell(1);
+	cell1.innerText="New Cell1";
+	cell2.innerText="New Cell2";
+		
+	
   
   
 }
